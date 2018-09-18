9f85610a3cf6600dcab55436870bd86e8e8aa1b8
github username <github email address>
2018-09-18 10:41:05 -0400

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
-// your ImageSlider code here!
+// <ImageSlider />
+//   ✓ should set the `currentSlideIndex` to 0 on its state
+//   ✓ render the correct text
+
+import React, { Component } from 'react'
+ class ImageSlider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+   render() {
+    return(
+      <div>I am on slide {this.state.currentSlideIndex}</div>
+    );
+  }
+}
+ export default ImageSlider;
